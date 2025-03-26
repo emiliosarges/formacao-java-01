9d448b2d81056312a17f24bafdd5a3cc4e863928
Emílio Silva <emiliosarges@gmail.com>
2025-03-26 00:19:01 -0300

    update

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô mundo!");
-        }
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Marverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.7 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento:
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2); //casting é a conversão explícita ou implícita de um tipo de dado para outro.
+        System.out.println(classificacao);
     }
+}
