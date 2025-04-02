839ff4c447a1f7bc30ce730e9bcb14bdb42c6ff6
Emílio Silva <emiliosarges@gmail.com>
2025-04-02 20:13:35 -0300

    estudos finalizados

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -20,14 +22,14 @@ public class Principal {
         //meuFilme.totalDeAvaliacoes = 1;
         //System.out.println(meuFilme.pegaMedia());
 
-        Serie serie = new Serie();
-        serie.setNome("Lost");
-        serie.setAnoDeLancamento(2000);
-        serie.exibeFichaTecnica();
-        serie.setTemporadas(10);
-        serie.setEpisodiosPorTemporada(10);
-        serie.setMinutosPorEpisodio(50);
-        System.out.println("Duração para maratonar a Série: " + serie.getDuracaoEmMinutos());
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar a Série: " + lost.getDuracaoEmMinutos());
 
         Filme outroFilme = new Filme();
         outroFilme.setNome("Avatar");
@@ -37,7 +39,17 @@ public class Principal {
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
         calculadora.inclui(meuFilme);
         calculadora.inclui(outroFilme);
-        calculadora.inclui(serie);
-        System.out.println(calculadora.getTempoTotal());
+        calculadora.inclui(lost);
+        System.out.println("Tempo para maratonar Filmes e Seria selecionadas: " + calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
     }
 }
